b9a7682aeaf2b7c29d21525bf17c3c05690cf2cb
Manu <jagirimanaswini@gmail.com>
2025-01-05 16:20:32 -0800

    conversation added

---
+++ 02-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=Manu
+PERSON2=Sri
+
+echo " $PERSON1: Hi srikanth"
+echo " $PERSON2 : Hi Manu "
+echo "$PERSON3: Let's meet today at 5pm"
---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+PERSON1=Manu
+PERSON2=Sri
+
+echo " $PERSON1=Hi"
+echo " $PERSON2=Hello"
