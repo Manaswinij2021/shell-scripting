11c5fd5b90c9a92fa6326bdf720b144efa94c0ca
Manu <jagirimanaswini@gmail.com>
2025-01-08 16:29:32 -0800

    shell-scripting

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -2,9 +2,10 @@
 
 NUMBER=$1
 
-if[ $Number -gt 100 ]
+if[ $NUMBER -gt 100 ]
 then
-echo "the number is more than 100"
+
+   echo "the number is more than 100"
 else
-echo  "the number is less than or greater than 100"
+   echo  "the number is less than or equal to 100"
 fi
